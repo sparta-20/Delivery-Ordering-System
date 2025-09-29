af303b4d630f280ce0f7e3a2b923054de3a9657f
alscksdlek <mseon916@naver.com>
2025-09-29 12:42:40 +0900

    feat/add User domain with JPA Auditing

---
+++ src/main/java/com/delivery/user/entity/User.java
@@ -0,0 +1,40 @@
+package com.delivery.user.entity;
+
+import com.delivery.common.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "p_user")
+@Getter
+public class User extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role = UserRoleEnum.CUSTOMER;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PublicStatus publicStatus = PublicStatus.PUBLIC;;
+
+    public User(String nickname, String email, String password) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+}
