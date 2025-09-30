788b3d63c59c8d3870d53b8b25d50ab0cc30e64b
alscksdlek <164968618+alscksdlek@users.noreply.github.com>
2025-09-30 14:01:53 +0900

    feat/#12/login

--- src/main/java/com/delivery/config/SecurityConfig.java
+++ src/main/java/com/delivery/config/SecurityConfig.java
@@ -1,24 +1,47 @@
 package com.delivery.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import com.delivery.jwt.JwtUtil;
+import com.delivery.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable());
 
-        // 임시 모든 요청 허용, 추후 JWT 적용 시 변경
+        //추후 경로 추가, ex) .requestMatchers("/stores/**", "/menus/**").hasRole("OWNER")
         http.authorizeHttpRequests((authorizeHttpRequests) ->
-                authorizeHttpRequests.
-                        anyRequest().permitAll()
+                authorizeHttpRequests
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/api/v1/auth/signup").permitAll()
+                        .requestMatchers("/api/v1/auth/login").permitAll()
+                        .anyRequest().authenticated()
         );
 
         http.formLogin((formLogin) -> formLogin.disable());
