72e374160ff3b45545e290859f4b5ea650adac59
Cho2unseo <escces00@gmail.com>
2025-09-30 23:17:52 +0900

    fix/find status order

--- src/main/java/com/delivery/order/repository/OrderRepository.java
+++ src/main/java/com/delivery/order/repository/OrderRepository.java
@@ -2,10 +2,15 @@ package com.delivery.order.repository;
 
 import com.delivery.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+    @Query("SELECT DISTINCT o FROM Order o " +
+            "JOIN o.orderMenus m " +
+            "WHERE o.user.userId = :userId " +
+            "AND m.status = com.delivery.order.entity.OrderMenuStatus.ORDER")
     List<Order> findByUser_UserId(Long userId);
 }
