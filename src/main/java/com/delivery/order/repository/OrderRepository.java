4f30c9358d12db590839bc2372e90161801b3f76
Cho2unseo <escces00@gmail.com>
2025-10-01 18:16:48 +0900

    fix/remove jpql

--- src/main/java/com/delivery/order/repository/OrderRepository.java
+++ src/main/java/com/delivery/order/repository/OrderRepository.java
@@ -8,9 +8,5 @@ import java.util.List;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-    @Query("SELECT DISTINCT o FROM Order o " +
-            "JOIN o.orderMenus m " +
-            "WHERE o.user.userId = :userId " +
-            "AND m.status = com.delivery.order.entity.OrderMenuStatus.ORDER")
     List<Order> findByUser_UserId(Long userId);
 }
