ca01358a873fc6fee02f9b332903be1c0619e098
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-10-02 14:07:22 +0900

    fix/user controller test
    1. document 의존성 제거
    2. withMockUser추가
    3. abstract ControllerTest 삭제

---
+++ src/test/java/com/delivery/ordering/WithMockCustomUser.java
@@ -0,0 +1,19 @@
+package com.delivery.ordering;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
+public @interface WithMockCustomUser {
+    
+    long id() default 1L;
+    
+    String nickname() default "testuser";
+    
+    String email() default "test@example.com";
+    
+    String role() default "CUSTOMER";
+}
--- src/test/java/com/delivery/ordering/user/controller/UserControllerTest.java
+++ src/test/java/com/delivery/ordering/user/controller/UserControllerTest.java
@@ -1,21 +1,33 @@
 package com.delivery.ordering.user.controller;
 
-import com.delivery.ordering.ControllerTest;
+import com.delivery.ordering.TestSecurityConfig;
 import com.delivery.ordering.WithMockCustomUser;
 import com.delivery.user.controller.UserController;
+import com.delivery.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = UserController.class)
-@AutoConfigureRestDocs
-class UserControllerTest extends ControllerTest {
+@WebMvcTest(controllers = {UserController.class})
+@Import(TestSecurityConfig.class)
+@ActiveProfiles("test")
+class UserControllerTest{
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
 
     @Test
     @DisplayName("내 정보를 조회할 수 있다")
