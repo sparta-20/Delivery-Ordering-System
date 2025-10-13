fe6a49c17cca54cd2ad5274a34199ce43c44c0b3
hoding <47974623+seokjun7410@users.noreply.github.com>
2025-10-13 15:51:47 +0900

    test/유저조회 (#48)

---
+++ src/test/java/com/delivery/user/service/UserServiceTest.java
@@ -0,0 +1,41 @@
+package com.delivery.user.service;
+
+import com.delivery.user.entity.User;
+import com.delivery.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UserServiceTest {
+    @MockitoBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    @DisplayName("나의 유저정보를 조회할 수 있다.")
+    void 나의_유저정보를_조회할_수_있다() {
+        long userId = 1L;
+
+        User user = User.builder()
+                .userId(userId)
+                .build();
+        user.cancelDeleted(user.getUserId());
+
+        when(userRepository.findByUserIdAndDeletedAtIsNull(userId))
+                .thenReturn(Optional.of(user));
+
+        User result = userService.getUserById(userId);
+
+        assertEquals(userId, result.getUserId());
+    }
+}
