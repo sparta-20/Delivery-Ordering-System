200701006dfafb181661ac8db60177cb3c862b6d
Cho2unseo <escces00@gmail.com>
2025-10-13 12:56:23 +0900

    chore

--- src/main/java/com/delivery/global/exception/ErrorCode.java
+++ src/main/java/com/delivery/global/exception/ErrorCode.java
@@ -19,7 +19,7 @@ public enum ErrorCode {
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "E005", "아이디 또는 비밀번호가 올바르지 않습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "A001", "유효하지 않은 토큰입니다."),
 
-    ITEM_REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "I001", "해당 상품을 찾을 수 없습니다");
+    ITEM_REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "I001", "해당 상품을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
