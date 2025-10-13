24d54a31fda316684044115a19d42327a136a62c
hoding <47974623+seokjun7410@users.noreply.github.com>
2025-10-13 15:46:25 +0900

    Feat/#23/유저조회 (#47)
    * test/유저조회

---
+++ src/main/java/com/delivery/domain/user/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.delivery.domain.user.controller;
+
+import com.delivery.common.ApiResponse;
+import com.delivery.security.UserDetailsImpl;
+import com.delivery.domain.user.dto.UserResponse;
+import com.delivery.user.entity.User;
+import com.delivery.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+@Slf4j
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getUserMe(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        User requester = userDetails.getUser();
+        User user = userService.getUserById(requester.getUserId());
+        return new ResponseEntity<>(ApiResponse.success(UserResponse.from(user)), HttpStatus.OK);
+    }
+}
