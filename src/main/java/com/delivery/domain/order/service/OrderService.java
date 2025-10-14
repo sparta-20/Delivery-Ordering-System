b7851d11585e1026b4a5bc84ae0524777dce6387
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-10-14 14:48:32 +0900

    Feat/#59/order cancel (#69)

--- src/main/java/com/delivery/domain/order/service/OrderService.java
+++ src/main/java/com/delivery/domain/order/service/OrderService.java
@@ -1,13 +1,17 @@
 package com.delivery.domain.order.service;
 
+import com.delivery.domain.order.dto.OrderRequestDto;
 import com.delivery.domain.order.dto.OrderResponseDto;
 import com.delivery.domain.order.entity.Order;
 import com.delivery.domain.user.entity.User;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface OrderService {
     List<OrderResponseDto.OrderListDto> getOrderList(Long userId);
     // 수정 필요
     List<OrderResponseDto.OrderListDto> getOrdersByOwner(Long ownerId);
+
+    void cancelOrder(Long userId, UUID orderId, OrderRequestDto.CancelOrderDto dto);
 }
