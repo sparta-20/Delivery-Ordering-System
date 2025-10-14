67780339d210af8746d73fc122339ab152a6cefb
alscksdlek <mseon916@naver.com>
2025-10-14 10:45:27 +0900

    refactor/objectMapper

--- src/main/java/com/delivery/global/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/delivery/global/security/filter/JwtAuthenticationFilter.java
@@ -38,7 +38,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         try {
-            LoginRequestDto requestDto = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDto.class);
+            LoginRequestDto requestDto = objectMapper.readValue(request.getInputStream(), LoginRequestDto.class);
 
             UsernamePasswordAuthenticationToken authenticationToken =
                     new UsernamePasswordAuthenticationToken(
