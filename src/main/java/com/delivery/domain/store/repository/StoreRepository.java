9aabe3a109444807e765c426859e4b1a7259500f
gosera <gosera0204@gmail.com>
2025-10-14 15:55:50 +0900

    Feat/#54/store master read (#100)
    * feat/read my store
    
    * feat/read owners store

--- src/main/java/com/delivery/domain/store/repository/StoreRepository.java
+++ src/main/java/com/delivery/domain/store/repository/StoreRepository.java
@@ -2,6 +2,8 @@ package com.delivery.domain.store.repository;
 
 import com.delivery.domain.store.entity.Store;
 import com.delivery.domain.store.entity.StoreStatusEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,5 +11,6 @@ import java.util.UUID;
 
 public interface StoreRepository extends JpaRepository<Store,UUID> {
 
-    Optional<Store> findByStoreIdAndStatus(UUID storeId, StoreStatusEnum storeStatusEnum);
+    Optional<Store> findByStoreIdAndStatus(UUID storeId, StoreStatusEnum status);
+    Page<Store> findAllByOwnerUserIdAndStatus(Long ownerUserId, StoreStatusEnum status, Pageable pageable);
 }
--- src/main/java/com/delivery/domain/store/service/StoreService.java
+++ src/main/java/com/delivery/domain/store/service/StoreService.java
@@ -4,6 +4,9 @@ import com.delivery.domain.store.dto.StoreCreateReq;
 import com.delivery.domain.store.dto.StoreRes;
 import com.delivery.domain.store.dto.StoreUpdateReq;
 import com.delivery.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.UUID;
 
 public interface StoreService {
@@ -16,6 +19,15 @@ public interface StoreService {
 
     // OWNER, MASTER - 가게 삭제
     StoreRes deleteStore(UUID storeId, User user);
+
+    // OWNER, MASTER - 본인 가게 조회
+    Page<StoreRes> getMyStores(User user, Pageable pageable);
+
+    // MASTER - 점주별 가게 조회
+    Page<StoreRes> getOwnerStores(Long ownerUserId, User user, Pageable pageable);
+
+
+
 
 
 
