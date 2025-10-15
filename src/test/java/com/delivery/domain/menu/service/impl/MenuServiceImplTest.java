83b5bcbaa1bccf23133d8d7a853a44d20dbfab80
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-10-15 13:56:55 +0900

    test/리팩토링

--- src/test/java/com/delivery/domain/menu/service/impl/MenuServiceImplTest.java
+++ src/test/java/com/delivery/domain/menu/service/impl/MenuServiceImplTest.java
@@ -32,54 +32,54 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 class MenuServiceImplTest {
 
-    @Autowired private MenuService menuService;
-    @MockitoBean private UserService userService;
-    @MockitoBean private StoreRepository storeRepository;
-    @MockitoBean private MenuRepository menuRepository;
+    @Autowired
+    private MenuService menuService;
+    @MockitoBean
+    private UserService userService;
+    @MockitoBean
+    private StoreRepository storeRepository;
+    @MockitoBean
+    private MenuRepository menuRepository;
 
     @Test
     @DisplayName("OWNBER는 자신의 가게에 메뉴를 생성할 수 있다.")
     void 메뉴를_생성할_수_있다() {
         long userId = 1L;
-        UUID storeId = UUID.randomUUID();
-        UUID menuId = UUID.randomUUID();
-        User user = User.builder().userId(userId).role(UserRoleEnum.OWNER).build();
-        Store store = Store.builder().storeId(storeId).status(StoreStatusEnum.ACTIVE).owner(user).build();
-        Menu menu = Menu.builder().menuId(menuId).status(MenuStatusEnum.AVAILABLE).store(store).build();
+        User owner = createUser(userId, UserRoleEnum.OWNER);
+        Store ownerStore = createStore(owner);
+        Menu menu = createMenu(ownerStore, MenuStatusEnum.AVAILABLE);
 
-        when(userService.getUserById(userId)).thenReturn(user);
-        when(storeRepository.findByStoreIdAndStatus(storeId, StoreStatusEnum.ACTIVE)).thenReturn(Optional.of(store));
+        when(userService.getUserById(userId)).thenReturn(owner);
+        when(storeRepository.findByStoreIdAndStatus(ownerStore.getStoreId(), StoreStatusEnum.ACTIVE)).thenReturn(Optional.of(ownerStore));
         when(menuRepository.save(any())).thenReturn(menu);
 
         CreateMenuReq req = new CreateMenuReq();
-        req.setStoreId(storeId);
+        req.setStoreId(ownerStore.getStoreId());
         MenuRes menuRes = menuService.create(userId, req);
 
-        assertEquals(menuId, menuRes.getMenuId());
-        assertEquals(storeId, menuRes.getStoreId());
+        assertEquals(menu.getMenuId(), menuRes.getMenuId());
+        assertEquals(ownerStore.getStoreId(), menuRes.getStoreId());
         assertEquals(MenuStatusEnum.AVAILABLE, menuRes.getStatus());
     }
 
     @Test
-    @DisplayName("Owner는 메뉴 생성할 때 나의 가게가 아닌경우 에외 발생")
+    @DisplayName("OWNER는 메뉴 생성할 때 나의 가게가 아닌 경우 에외 발생")
     void 메뉴_생성할_때_나의_가게가_아닌경우_에외_발생() {
-        UserRoleEnum role = UserRoleEnum.CUSTOMER;
+        UserRoleEnum role = UserRoleEnum.OWNER;
         long userId = 1L;
-        User user = User.builder().role(role).userId(userId).build();
-        User otherStoreOwner = User.builder().userId(2L).build();
-
-        UUID storeId = UUID.randomUUID();
-        UUID menuId = UUID.randomUUID();
-        Store otherStore = Store.builder().storeId(storeId).status(StoreStatusEnum.ACTIVE).owner(otherStoreOwner).build();
-        Menu menu = Menu.builder().menuId(menuId).status(MenuStatusEnum.AVAILABLE).store(otherStore).build();
+        long otherUser = 2L;
+        User user = createUser(userId, role);
+        User otherStoreOwner = createUser(otherUser, role);
+        Store otherStore = createStore(otherStoreOwner);
+        Menu menu = createMenu(otherStore, MenuStatusEnum.AVAILABLE);
 
         when(userService.getUserById(userId)).thenReturn(user);
-        when(storeRepository.findByStoreIdAndStatus(storeId, StoreStatusEnum.ACTIVE)).thenReturn(Optional.of(otherStore));
+        when(storeRepository.findByStoreIdAndStatus(otherStore.getStoreId(), StoreStatusEnum.ACTIVE)).thenReturn(Optional.of(otherStore));
         when(menuRepository.save(any())).thenReturn(menu);
 
         BusinessException businessException = assertThrows(BusinessException.class, () -> {
             CreateMenuReq req = new CreateMenuReq();
-            req.setStoreId(storeId);
+            req.setStoreId(otherStore.getStoreId());
             menuService.create(userId, req);
         });
 
@@ -90,25 +90,21 @@ class MenuServiceImplTest {
     @DisplayName("MASTER는 모든 Store의 메뉴를 생성할 수 있다.")
     void MASTER는_모든_Store의_메뉴를_생성할_수_있다() {
         UserRoleEnum role = UserRoleEnum.MASTER;
-        long userId = 1L;
-        User user = User.builder().role(role).userId(userId).build();
-        User otherStoreOwner = User.builder().userId(2L).build();
-
-        UUID storeId = UUID.randomUUID();
-        UUID menuId = UUID.randomUUID();
-        Store otherStore = Store.builder().storeId(storeId).status(StoreStatusEnum.ACTIVE).owner(otherStoreOwner).build();
-        Menu menu = Menu.builder().menuId(menuId).status(MenuStatusEnum.AVAILABLE).store(otherStore).build();
+        long userId = 1L; long otherStoreUserId = 2L;
+        User user = createUser(userId, role);
+        Store otherStore = createStore(createUser(otherStoreUserId, UserRoleEnum.OWNER));
+        Menu menu = createMenu(otherStore, MenuStatusEnum.AVAILABLE);
 
         when(userService.getUserById(userId)).thenReturn(user);
-        when(storeRepository.findByStoreIdAndStatus(storeId, StoreStatusEnum.ACTIVE)).thenReturn(Optional.of(otherStore));
+        when(storeRepository.findByStoreIdAndStatus(otherStore.getStoreId(), StoreStatusEnum.ACTIVE)).thenReturn(Optional.of(otherStore));
         when(menuRepository.save(any())).thenReturn(menu);
 
         CreateMenuReq req = new CreateMenuReq();
-        req.setStoreId(storeId);
+        req.setStoreId(otherStore.getStoreId());
         MenuRes menuRes = menuService.create(userId, req);
 
-        assertEquals(menuId, menuRes.getMenuId());
-        assertEquals(storeId, menuRes.getStoreId());
+        assertEquals(menu.getMenuId(), menuRes.getMenuId());
+        assertEquals(otherStore.getStoreId(), menuRes.getStoreId());
         assertEquals(MenuStatusEnum.AVAILABLE, menuRes.getStatus());
     }
 
@@ -116,45 +112,39 @@ class MenuServiceImplTest {
     @DisplayName("MANAGER는 모든 Store의 메뉴를 생성할 수 있다.")
     void MANAGER는_모든_Store의_메뉴를_생성할_수_있다() {
         UserRoleEnum role = UserRoleEnum.MASTER;
-        long userId = 1L;
-        User user = User.builder().role(role).userId(userId).build();
-        User otherStoreOwner = User.builder().userId(2L).build();
-
-        UUID storeId = UUID.randomUUID();
-        UUID menuId = UUID.randomUUID();
-        Store otherStore = Store.builder().storeId(storeId).status(StoreStatusEnum.ACTIVE).owner(otherStoreOwner).build();
-        Menu menu = Menu.builder().menuId(menuId).status(MenuStatusEnum.AVAILABLE).store(otherStore).build();
+        long userId = 1L; long otherUserId = 2L;
+        User user = createUser(userId, role);
+        Store otherStore = createStore(createUser(otherUserId, UserRoleEnum.OWNER));
+        Menu menu = createMenu(otherStore, MenuStatusEnum.AVAILABLE);
 
         when(userService.getUserById(userId)).thenReturn(user);
-        when(storeRepository.findByStoreIdAndStatus(storeId, StoreStatusEnum.ACTIVE)).thenReturn(Optional.of(otherStore));
+        when(storeRepository.findByStoreIdAndStatus(otherStore.getStoreId(), StoreStatusEnum.ACTIVE)).thenReturn(Optional.of(otherStore));
         when(menuRepository.save(any())).thenReturn(menu);
 
         CreateMenuReq req = new CreateMenuReq();
-        req.setStoreId(storeId);
+        req.setStoreId(otherStore.getStoreId());
         MenuRes menuRes = menuService.create(userId, req);
 
-        assertEquals(menuId, menuRes.getMenuId());
-        assertEquals(storeId, menuRes.getStoreId());
+        assertEquals(menu.getMenuId(), menuRes.getMenuId());
+        assertEquals(otherStore.getStoreId(), menuRes.getStoreId());
         assertEquals(MenuStatusEnum.AVAILABLE, menuRes.getStatus());
     }
 
     @Test
-    @DisplayName("CUSTOMER는 숨김(HIDDEN) 메뉴를 싱세 조회하면 예외 반환")
+    @DisplayName("CUSTOMER는 숨김(HIDDEN) 메뉴를 상세 조회하면 예외 반환")
     void customerCannotViewHiddenMenu() {
-        long userId = 1L;
-        User customer = User.builder().userId(userId).role(UserRoleEnum.CUSTOMER).build();
-        User owner = User.builder().userId(2L).build();
-        UUID menuId = UUID.randomUUID();
-        UUID storeId = UUID.randomUUID();
+        long userId = 1L; long ownerId = 2L;
+        User customer = createUser(userId, UserRoleEnum.CUSTOMER);
 
-        Store store = Store.builder().storeId(storeId).owner(owner).status(StoreStatusEnum.ACTIVE).build();
-        Menu hiddenMenu = Menu.builder().menuId(menuId).store(store).status(MenuStatusEnum.HIDDEN).build();
+        User owner = createUser(ownerId, UserRoleEnum.OWNER);
+        Store store = createStore(owner);
+        Menu hiddenMenu = createMenu(store, MenuStatusEnum.HIDDEN);
 
         when(userService.getUserById(userId)).thenReturn(customer);
-        when(menuRepository.findById(menuId)).thenReturn(Optional.of(hiddenMenu));
+        when(menuRepository.findById(hiddenMenu.getMenuId())).thenReturn(Optional.of(hiddenMenu));
 
         BusinessException exception = assertThrows(BusinessException.class, () ->
-                menuService.findMenuResById(userId, menuId)
+                menuService.findMenuResById(userId, hiddenMenu.getMenuId())
         );
         assertEquals(ErrorCode.MENU_NOT_FOUND, exception.getErrorCode());
     }
@@ -162,64 +152,38 @@ class MenuServiceImplTest {
     @Test
     @DisplayName("OWNER는 자신의 가게 숨김 메뉴 상세 조회할 수 있다.")
     void ownerCanViewHiddenMenu() {
-        long userId = 2L;
-        User owner = User.builder().userId(userId).role(UserRoleEnum.OWNER).build();
-        UUID menuId = UUID.randomUUID();
-        UUID storeId = UUID.randomUUID();
-
-        Store store = Store.builder().storeId(storeId).owner(owner).status(StoreStatusEnum.ACTIVE).build();
-        Menu hiddenMenu = Menu.builder().menuId(menuId).store(store).status(MenuStatusEnum.HIDDEN).build();
+        long userId = 1L;
+        User owner = createUser(userId, UserRoleEnum.OWNER);
+        Store store = createStore(owner);
+        Menu hiddenMenu = createMenu(store, MenuStatusEnum.HIDDEN);
 
         when(userService.getUserById(userId)).thenReturn(owner);
-        when(menuRepository.findById(menuId)).thenReturn(Optional.of(hiddenMenu));
+        when(menuRepository.findById(hiddenMenu.getMenuId())).thenReturn(Optional.of(hiddenMenu));
 
-        MenuRes menuRes = menuService.findMenuResById(userId, menuId);
+        MenuRes menuRes = menuService.findMenuResById(userId, hiddenMenu.getMenuId());
 
-        assertEquals(menuId, menuRes.getMenuId());
-        assertEquals(storeId, menuRes.getStoreId());
+        assertEquals(hiddenMenu.getMenuId(), menuRes.getMenuId());
+        assertEquals(store.getStoreId(), menuRes.getStoreId());
     }
 
-    @Test
-    @DisplayName("MANAGER/MASTER는 모든 메뉴 상세 조회 할 수 있다.")
-    void managerOrMasterCanViewHiddenMenu() {
-        long userId = 3L;
-        User manager = User.builder().userId(userId).role(UserRoleEnum.MANAGER).build();
-        UUID menuId = UUID.randomUUID();
-        UUID storeId = UUID.randomUUID();
-
-        User owner = User.builder().userId(4L).build();
-        Store store = Store.builder().storeId(storeId).owner(owner).status(StoreStatusEnum.ACTIVE).build();
-        Menu hiddenMenu = Menu.builder().menuId(menuId).store(store).status(MenuStatusEnum.HIDDEN).build();
-
-        when(userService.getUserById(userId)).thenReturn(manager);
-        when(menuRepository.findById(menuId)).thenReturn(Optional.of(hiddenMenu));
-
-        MenuRes menuRes = menuService.findMenuResById(userId, menuId);
-
-        assertEquals(menuId, menuRes.getMenuId());
-        assertEquals(storeId, menuRes.getStoreId());
-    }
 
     @Test
     @DisplayName("CUSTOMER는 메뉴 수정시 예외 발생")
     void customerCannotUpdateMenu() {
-        long userId = 1L;
-        User customer = User.builder().userId(userId).role(UserRoleEnum.CUSTOMER).build();
-        User owner = User.builder().userId(2L).build();
-        UUID menuId = UUID.randomUUID();
-        UUID storeId = UUID.randomUUID();
-
-        Store store = Store.builder().storeId(storeId).owner(owner).status(StoreStatusEnum.ACTIVE).build();
-        Menu menu = Menu.builder().menuId(menuId).store(store).status(MenuStatusEnum.AVAILABLE).build();
+        long userId = 1L; long ownerId = 2L;
+        User customer = createUser(userId, UserRoleEnum.CUSTOMER);
+        User owner = createUser(ownerId, UserRoleEnum.OWNER);
+        Store store = createStore(owner);
+        Menu menu = createMenu(store, MenuStatusEnum.AVAILABLE);
 
         when(userService.getUserById(userId)).thenReturn(customer);
-        when(menuRepository.findById(menuId)).thenReturn(Optional.of(menu));
+        when(menuRepository.findById(menu.getMenuId())).thenReturn(Optional.of(menu));
 
         UpdateMenuReq req = new UpdateMenuReq();
         req.setName("변경");
 
         BusinessException exception = assertThrows(BusinessException.class, () ->
-                menuService.update(req, userId, menuId)
+                menuService.update(req, userId, menu.getMenuId())
         );
         assertEquals(ErrorCode.FORBIDDEN_READ_STORE, exception.getErrorCode());
     }
@@ -227,62 +191,84 @@ class MenuServiceImplTest {
     @Test
     @DisplayName("OWNER는 자기 가게 메뉴 수정할 수 있다.")
     void ownerCanUpdateOwnMenu() {
-        long userId = 2L;
-        User owner = User.builder().userId(userId).role(UserRoleEnum.OWNER).build();
-        UUID menuId = UUID.randomUUID();
-        UUID storeId = UUID.randomUUID();
-
-        Store store = Store.builder().storeId(storeId).owner(owner).status(StoreStatusEnum.ACTIVE).build();
-        Menu menu = Menu.builder().menuId(menuId).store(store).status(MenuStatusEnum.AVAILABLE).build();
+        long userId = 1L;
+        User owner = createUser(userId, UserRoleEnum.OWNER);
+        Store store = createStore(owner);
+        Menu menu = createMenu(store, MenuStatusEnum.AVAILABLE);
 
         when(userService.getUserById(userId)).thenReturn(owner);
-        when(menuRepository.findById(menuId)).thenReturn(Optional.of(menu));
+        when(menuRepository.findById(menu.getMenuId())).thenReturn(Optional.of(menu));
 
         UpdateMenuReq req = new UpdateMenuReq();
         req.setName("변경");
 
-        MenuRes updated = menuService.update(req, userId, menuId);
+        MenuRes updated = menuService.update(req, userId, menu.getMenuId());
 
         assertEquals("변경", updated.getName());
-        assertEquals(menuId, updated.getMenuId());
+        assertEquals(menu.getMenuId(), updated.getMenuId());
     }
 
     @Test
-    @DisplayName("OWNER는 자기 가게 메뉴 조회할 수 있다. (숨김 포함)")
+    @DisplayName("OWNER는 자기 가게 숨김 메뉴 조회할 수 있다.")
     void ownerCanViewOwnMenu() {
-        long userId = 2L;
-        User owner = User.builder().userId(userId).role(UserRoleEnum.OWNER).build();
-        UUID menuId = UUID.randomUUID();
-        UUID storeId = UUID.randomUUID();
-
-        Store store = Store.builder().storeId(storeId).owner(owner).status(StoreStatusEnum.ACTIVE).build();
-        Menu hiddenMenu = Menu.builder().menuId(menuId).store(store).status(MenuStatusEnum.HIDDEN).build();
+        long userId = 1L;
+        User owner = createUser(userId, UserRoleEnum.OWNER);
+        Store store = createStore(owner);
+        Menu hiddenMenu = createMenu(store, MenuStatusEnum.HIDDEN);
 
         when(userService.getUserById(userId)).thenReturn(owner);
-        when(menuRepository.findById(menuId)).thenReturn(Optional.of(hiddenMenu));
+        when(menuRepository.findById(hiddenMenu.getMenuId())).thenReturn(Optional.of(hiddenMenu));
 
-        MenuRes result = menuService.findMenuResById(userId, menuId);
+        MenuRes result = menuService.findMenuResById(userId, hiddenMenu.getMenuId());
 
-        assertEquals(menuId, result.getMenuId());
+        assertEquals(hiddenMenu.getMenuId(), result.getMenuId());
     }
 
     @Test
-    @DisplayName("MANAGER/MASTER는 모든 메뉴 조회할 수 있다.")
-    void managerOrMasterCanViewAllMenus() {
-        long userId = 3L;
-        User manager = User.builder().userId(userId).role(UserRoleEnum.MANAGER).build();
-        User owner = User.builder().userId(4L).build();
-        UUID menuId = UUID.randomUUID();
-        UUID storeId = UUID.randomUUID();
-
-        Store store = Store.builder().storeId(storeId).owner(owner).status(StoreStatusEnum.ACTIVE).build();
-        Menu hiddenMenu = Menu.builder().menuId(menuId).store(store).status(MenuStatusEnum.HIDDEN).build();
+    @DisplayName("MANAGER는 숨김 메뉴 조회할 수 있다.")
+    void managerViewAllMenus() {
+        long userId = 1L; long ownerId = 2L;
+        User manager = createUser(userId, UserRoleEnum.MANAGER);
+        User owner = createUser(ownerId, UserRoleEnum.OWNER);
+        Store store = createStore(owner);
+        Menu hiddenMenu = createMenu(store, MenuStatusEnum.HIDDEN);
 
         when(userService.getUserById(userId)).thenReturn(manager);
-        when(menuRepository.findById(menuId)).thenReturn(Optional.of(hiddenMenu));
+        when(menuRepository.findById(hiddenMenu.getMenuId())).thenReturn(Optional.of(hiddenMenu));
 
-        MenuRes result = menuService.findMenuResById(userId, menuId);
+        MenuRes result = menuService.findMenuResById(userId, hiddenMenu.getMenuId());
 
-        assertEquals(menuId, result.getMenuId());
+        assertEquals(hiddenMenu.getMenuId(), result.getMenuId());
+    }
+
+    @Test
+    @DisplayName("MASTER는 숨김 메뉴 조회할 수 있다.")
+    void masterCanViewAllMenus() {
+        long userId = 1L; long ownerId = 2L;
+        User manager = createUser(userId, UserRoleEnum.MASTER);
+        User owner = createUser(ownerId, UserRoleEnum.OWNER);
+        Store store = createStore(owner);
+        Menu hiddenMenu = createMenu(store, MenuStatusEnum.HIDDEN);
+
+        when(userService.getUserById(userId)).thenReturn(manager);
+        when(menuRepository.findById(hiddenMenu.getMenuId())).thenReturn(Optional.of(hiddenMenu));
+
+        MenuRes result = menuService.findMenuResById(userId, hiddenMenu.getMenuId());
+
+        assertEquals(hiddenMenu.getMenuId(), result.getMenuId());
+    }
+
+    private static User createUser(long userId, UserRoleEnum role) {
+        return User.builder().userId(userId).role(role).build();
+    }
+
+    private static Menu createMenu(Store store, MenuStatusEnum menuStatusEnum) {
+        UUID menuId = UUID.randomUUID();
+        return Menu.builder().menuId(menuId).store(store).status(menuStatusEnum).build();
+    }
+
+    private static Store createStore(User owner) {
+        UUID storeId = UUID.randomUUID();
+        return Store.builder().storeId(storeId).owner(owner).status(StoreStatusEnum.ACTIVE).build();
     }
 }
