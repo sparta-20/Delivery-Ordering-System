4376169140346483428a4e4900fa45a03ec54e96
alscksdlek <164968618+alscksdlek@users.noreply.github.com>
2025-10-15 12:50:17 +0900

    Feat/#99/logout (#108)
    * feat/implement logout with blacklist
    
    * feat/add scheduler to delete expired tokens
    
    * feat/ont to one relationship between User and RefreshToken
    
    * feat/add validation signup

--- src/main/java/com/delivery/domain/auth/controller/AuthController.java
+++ src/main/java/com/delivery/domain/auth/controller/AuthController.java
@@ -1,16 +1,15 @@
 package com.delivery.domain.auth.controller;
 
-import com.delivery.domain.auth.dto.SignUpRequestDto;
+import com.delivery.domain.auth.dto.SignUpReq;
 import com.delivery.domain.auth.service.AuthServiceImpl;
 import com.delivery.global.common.ApiResponse;
+import com.delivery.global.jwt.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -18,16 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthServiceImpl authService;
+    private final JwtUtil jwtUtil;
 
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponse<Void>> signup(@RequestBody SignUpRequestDto signUpRequestDto) {
-        authService.signup(signUpRequestDto);
+    public ResponseEntity<ApiResponse<Void>> signup(@Validated @RequestBody SignUpReq signUpReq) {
+        authService.signup(signUpReq);
         return ResponseEntity.ok(ApiResponse.success(null));
     }
 
     @PostMapping("/refresh")
     public ResponseEntity<ApiResponse<Void>> refreshAccessToken(HttpServletRequest request, HttpServletResponse response) {
         authService.updateRefreshAccessToken(request, response);
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<ApiResponse<Void>> logout(
+            @CookieValue(value = "accessToken", required = false) String accessToken,
+            HttpServletResponse response
+    ) {
+        authService.logout(accessToken);
+        jwtUtil.expireCookie(response);
         return ResponseEntity.ok(ApiResponse.success(null));
     }
 }
--- src/main/java/com/delivery/domain/auth/service/AuthService.java
+++ src/main/java/com/delivery/domain/auth/service/AuthService.java
@@ -1,11 +1,14 @@
 package com.delivery.domain.auth.service;
 
-import com.delivery.domain.auth.dto.SignUpRequestDto;
+import com.delivery.domain.auth.dto.SignUpReq;
+import com.delivery.domain.user.entity.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface AuthService {
-    void signup(SignUpRequestDto signUpRequestDto);
-    void saveOrUpdateRefreshToken(Long userId, String refreshToken);
+    void signup(SignUpReq signUpReq);
+    void saveOrUpdateRefreshToken(User user, String refreshToken);
     void updateRefreshAccessToken(HttpServletRequest request, HttpServletResponse response);
+    void logout(String accessToken);
+    boolean isBlacklisted(String accessToken);
 }
