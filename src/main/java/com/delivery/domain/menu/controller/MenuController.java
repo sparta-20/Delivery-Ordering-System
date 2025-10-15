40ac18799ff4d925afa955b07b563f800ee4d7c1
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-10-15 16:32:54 +0900

    feat/menu-list

--- src/main/java/com/delivery/domain/menu/controller/MenuController.java
+++ src/main/java/com/delivery/domain/menu/controller/MenuController.java
@@ -9,6 +9,10 @@ import com.delivery.global.common.ApiResponse;
 import com.delivery.global.security.service.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -44,7 +48,19 @@ public class MenuController {
             @PathVariable(value = "menuId") UUID menuId
     ) {
         User user = userDetails.getUser();
-        MenuRes menuRes = menuService.findMenuResById(user.getUserId(), menuId);
+        MenuRes menuRes = menuService.getMenuResById(user.getUserId(), menuId);
+        return ResponseEntity.ok(ApiResponse.success(menuRes));
+    }
+
+    //메뉴 목록 조회
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<MenuRes>>> getMenuList(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestParam(value = "storeId") UUID storeId,
+            @PageableDefault(size = 10, page = 0, sort = "menuId", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        User user = userDetails.getUser();
+        Page<MenuRes> menuRes = menuService.getMenuResListByStoreId(user.getUserId(), storeId, pageable);
         return ResponseEntity.ok(ApiResponse.success(menuRes));
     }
 
@@ -72,7 +88,4 @@ public class MenuController {
         menuService.delete(user.getUserId(), menuId);
         return ResponseEntity.noContent().build();
     }
-
-    //메뉴 목록 조회
-
 }
