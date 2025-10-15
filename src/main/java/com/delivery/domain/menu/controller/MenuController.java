d606eeba7a512027dcc78afbeb07932538153da3
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-10-15 15:41:41 +0900

    fix/NotEmpty -> NotNull

--- src/main/java/com/delivery/domain/menu/controller/MenuController.java
+++ src/main/java/com/delivery/domain/menu/controller/MenuController.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/menus")
+@RequestMapping("/api/v1/menus")
 public class MenuController {
 
     private final MenuService menuService;
