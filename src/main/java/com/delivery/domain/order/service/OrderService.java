987fac42aab409a1b770a400c07eeab62d9b6cd3
김부경 <sa01023@naver.com>
2025-10-16 02:26:59 +0900

    feat/Soft Delete 반영 주문 조회 로직 구현

--- src/main/java/com/delivery/domain/order/service/OrderService.java
+++ src/main/java/com/delivery/domain/order/service/OrderService.java
@@ -2,6 +2,7 @@ package com.delivery.domain.order.service;
 
 import com.delivery.domain.order.dto.OrderReq;
 import com.delivery.domain.order.dto.OrderRes;
+import com.delivery.domain.order.entity.Order;
 
 import java.util.List;
 import java.util.UUID;
@@ -14,4 +15,6 @@ public interface OrderService {
     List<OrderRes.AllOrderListDto> getAllList();
     void cancelOrder(Long userId, UUID orderId, OrderReq.CancelOrderDto dto);
     OrderRes.OrderDetailDto getOrderDetail(Long userId, UUID orderId);
+    // 주문 단건 조회 (Soft Delete)
+    Order getOrderById(UUID orderId);
 }
