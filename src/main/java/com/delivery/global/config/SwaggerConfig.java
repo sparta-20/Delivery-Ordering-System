a1c156b73556614cda267c7bb0387ec5d19c8ac7
Cho2unseo <escces00@gmail.com>
2025-10-19 04:13:55 +0900

    fix/cors

--- src/main/java/com/delivery/global/config/SwaggerConfig.java
+++ src/main/java/com/delivery/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,7 +27,8 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .components(components)
                 .info(apiInfo())
-                .addSecurityItem(securityRequirement);
+                .addSecurityItem(securityRequirement)
+                .addServersItem(new Server().url("/"));
     }
 
     private Info apiInfo() {
