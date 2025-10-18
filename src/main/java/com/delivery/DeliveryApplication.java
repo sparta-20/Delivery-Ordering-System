488a346613f0b14886349bcf19382a9656a0cb9b
최은서 <104536578+Cho2unseo@users.noreply.github.com>
2025-10-19 05:08:10 +0900

    fix/cors (#166)

--- src/main/java/com/delivery/DeliveryApplication.java
+++ src/main/java/com/delivery/DeliveryApplication.java
@@ -1,5 +1,7 @@
 package com.delivery;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class DeliveryApplication {
 
